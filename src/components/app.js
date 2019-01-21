48347688b49309836354cce682210f1876bd8ea7
Cidojo <cidell@mail.ru>
2019-01-21 22:32:38 +0300

    first touch with react components

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './../style/app.css';
 import {hot} from 'react-hot-loader';
-
+import UserNameInput from './user-name-input';
 
 class App extends React.Component {
   render() {
     return (
       <div className="app">
         <h1>{`Chat Messenger`}</h1>
+        <UserNameInput />
       </div>
     );
   }
