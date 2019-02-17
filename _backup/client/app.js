3e81594dc42e94bc526970a0ee30ce0ab0540326
Cidojo <cidell@mail.ru>
2019-02-17 16:31:19 +0300

    removing _backup

--- _backup/client/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import UsernameForm from './components/username-form';
-import Chat from './chat';
-
-
-ReactDOM.render(<Chat />, document.getElementById('root'));
-// ReactDOM.render(<UsernameForm />, document.getElementById('root'));
